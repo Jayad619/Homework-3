0428e725a0a43a6854b7a798264b31897d542628
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-04-29 19:48:25 +0100

    Prompts

--- Develop/script.js
+++ Develop/script.js
@@ -7,10 +7,30 @@ const capitals = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P
 //array of lowercase letters
 const lowers = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 //array of specials characters
-const specials = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",":",";","'","\","|","<",",",",".",">",".","?","/"]
-function generatePassword() {
+const specials = ["!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",":",";","'","\","|","<",",",",".",">",".","?","/"];
+//promtps:
+//password length
 
- console.log("Hey you clicked") 
+function generatePassword() {
+let choices = [];
+var passwordlength = prompt("Please choose your length between 8 and 128 characters");
+
+if (passwordlength < 8 || passwordlength > 128){
+  alert("Please enter a number between 8 and 128");
+  generatePassword();
+}
+//Number criteria prompt
+var numbersPrompt = confirm("Would you like numbers in your new password?");
+
+//Uppercase prompt
+var uppercasePrompt = confirm("Would you like to include uppercase characters in your new password?");
+
+//Lowercase prompt
+var lowercasePrompt = confirm("Would you like to include lowercase characters in your new password?");
+
+//Special characters prompt
+var specialcharactersPrompt = confirm("Would you like to include any special characters in your password? eg. !££$%^ etc");
+
 }
  
 // Write password to the #password input
